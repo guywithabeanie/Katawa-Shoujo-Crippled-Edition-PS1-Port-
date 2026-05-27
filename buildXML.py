14b139dc4df261bd9f0893f5f5cc9bc9514526c7
cleabent <101577033+cleabent@users.noreply.github.com>
2026-05-28 00:52:02 +0900

    First attempt
    Did work fine, I'm not sure if it's good enough yet.
    Also bloated the buildXML just to annoy you, mike.

--- buildXML.py
+++ buildXML.py
@@ -1,92 +1,105 @@
-import xml.etree.ElementTree as ElementTree
-import os
+from argparse import ArgumentParser
 from pathlib import Path
-import sys
+import xml.etree.ElementTree as ET
+import os
 
-def recursivelyParseFolder( directory: Path, parentNode: ElementTree.SubElement ):
-    for x in os.listdir( directory ):
+
+def parse_folders(root: Path, directory: Path, parentNode: ET.SubElement):
+    """
+    Recursively adds files/directories to the parent node.
+
+    Args:
+        root (pathlib.Path): The root directory for the script.
+        directory (pathlib.Path): The directory to parse.
+        node (xml.etree.ET.SubElement): The parenting node to append to.
+    """
+    for x in os.listdir(directory):
         path = directory / x
 
-        #Remove the root directory from the path
-        useablePath = Path( *path.parts[1:] )
+        # Remove the root directory from the path
+        useablePath = path.resolve().relative_to(root.resolve())
 
         if path.is_file() and path.suffix != ".xml":
-            file = ElementTree.SubElement(parentNode, "file", {
-                "name" : x,
-                "type" : "data",
-                "source" : str( useablePath )
-            })
-        
+            file = ET.SubElement(
+                parentNode,
+                "file",
+                {"name": x, "type": "data", "source": str(useablePath)},
+            )
+
         elif path.is_dir():
-            dir = ElementTree.SubElement(parentNode, "dir", {
-                "name" : x,
-                "type" : "data",
-                "source" : str( useablePath )
-            })
-            recursivelyParseFolder( path, dir if any( path.iterdir() ) else parentNode )
+            dir = ET.SubElement(
+                parentNode,
+                "dir",
+                {"name": x, "type": "data", "source": str(useablePath)},
+            )
+            parse_folders(root, path, dir if any(path.iterdir()) else parentNode)
 
-    return
 
-config = {
-    "directory" : "",
-    "projectName" : "",
-    "volume" : "PS1DISK",
-    "publisher" : "MINGEBAG"
-}
+def parse_cmd():
 
-argv = sys.argv
-i = 1
+    parser = ArgumentParser(
+        prog="buildXML.py", description="Generates XML files for use with mkpsxiso."
+    )
 
-while i < len( argv ):
-    arg = argv[i]
+    parser.add_argument("-c", "--cd", default=Path("CDROM"), type=Path)
+    parser.add_argument("-prob", "--project", default="katawa", type=str)
+    parser.add_argument("-v", "--volume", default="KatawaShoujo", type=str)
+    parser.add_argument("-pub", "--publisher", default="4LeafStudios", type=str)
+    parser.add_argument(
+        "-o", "--output", default=Path("CDROM/isoconfig.xml"), type=Path
+    )
 
-    if arg in ("-cd", "-proj", "-v", "-pub"):
-        i += 1
-        if i >= len(argv):
-            raise ValueError(f"Missing value for {arg}")
+    args = parser.parse_args()
 
-        if arg == "-cd":
-            config["directory"] = argv[i]
-        elif arg == "-proj":
-            config["projectName"] = argv[i]
-        elif arg == "-v":
-            config["volume"] = argv[i]
-        elif arg == "-pub":
-            config["publisher"] = argv[i]
+    return {
+        "directory": args.cd,
+        "projectName": args.project,
+        "volume": args.volume,
+        "publisher": args.publisher,
+        "output": args.output
+    }
 
-    i += 1
 
-if config["directory"] == "" or config["projectName"] == "":
-    print("Usage : generateXML.py -cd CdDirectory -proj ProjectName [-v VolumeName] [-pub Publisher]")
-    sys.exit(1)
+def main():
 
-root = ElementTree.Element("iso_project", {
-    "image_name" : f"bin/{config['projectName']}.bin",
-    "cue_sheet"  : f"bin/{config['projectName']}.cue"
-})
+    config = parse_cmd()
 
-tracks = ElementTree.SubElement(root, "track", {
-    "type" : "data"
-})
+    rootDirectory = Path(config["directory"])
+    print(rootDirectory)
 
-identifiers = ElementTree.SubElement(tracks, "identifiers", {
-	"system"        : "PLAYSTATION",
-	"application"   : "PLAYSTATION",
-	"volume"        : config["volume"],
-	"volume_set"    : config["volume"],
-	"publisher"     : config["publisher"],
-	"data_preparer" : "MKPSXISO"
-})
+    root = ET.Element(
+        "iso_project",
+        {
+            "image_name": f"bin/{config['projectName']}.bin",
+            "cue_sheet": f"bin/{config['projectName']}.cue",
+        },
+    )
 
-directoryTree = ElementTree.SubElement(tracks, "directory_tree")
+    tracks = ET.SubElement(root, "track", {"type": "data"})
 
-rootDirectory = Path( config["directory"] )
-print( rootDirectory )
+    identifiers = ET.SubElement(
+        tracks,
+        "identifiers",
+        {
+            "system": "PLAYSTATION",
+            "application": "PLAYSTATION",
+            "volume": config["volume"],
+            "volume_set": config["volume"],
+            "publisher": config["publisher"],
+            "data_preparer": "MKPSXISO",
+        },
+    )
 
-recursivelyParseFolder( rootDirectory, directoryTree )
+    directoryTree = ET.SubElement(tracks, "directory_tree")
 
-tree = ElementTree.ElementTree(root)
+    parse_folders(rootDirectory, rootDirectory, directoryTree)
 
-ElementTree.indent(root, space="    ")
+    tree = ET.ElementTree(root)
 
-tree.write( f"{config['directory']}/isoconfig.xml", xml_declaration=True, encoding="UTF-8" )
+    ET.indent(root, space="    ")
+
+    tree.write(config["output"], xml_declaration=True, encoding="UTF-8")
+
+
+if __name__ == "__main__":
+    main()
